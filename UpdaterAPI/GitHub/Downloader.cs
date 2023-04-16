12d0e0d022eab74eeb4687eea40b814dc85d4011
Camyil-89 <zhukovkd@yandex.ru>
2023-04-16 23:18:29 +0300

    Добавьте файлы проекта.

---
+++ UpdaterAPI/GitHub/Downloader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdaterAPI.GitHub
+{
+	public static class Downloader
+	{
+		private static WebClient WebClient = new WebClient();
+
+
+		public static void SetToken(string token)
+		{
+			WebClient WebClient = new WebClient();
+			WebClient.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
+			WebClient.Headers.Add(HttpRequestHeader.Authorization, $"token {token}");
+			WebClient.Headers.Add(HttpRequestHeader.Accept, "application/octet-stream");
+		}
+
+		public static string GetLastVersion()
+		{
+			return "";
+		}
+	}
+}
